20249c7b4470815a187a2b0af68c4534bedc94a9
--replace-all <yiliu.lu@mail.mcgill.ca>
2023-02-11 14:54:49 -0500

    initial commit

---
+++ Datastream_automatic_update.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  7 18:57:35 2023
+
+@author: Lucie Lu: yiliu.lu@mail.mcgill.ca
+
+This is a python script that downloads daily stock return data from Datastream using Excel request table
+
+Previously I calculated stock market variables using Compustat Global, but since some firm-level variables are only
+available in Datastream, here I also calculate market measures using Datastream
+
+Downloading higher frequency data from Datastream excel needs to be done in batches.
+
+Here the example is daily return, and querey is run for each year.
+
+"""
+import win32com.client as win32 
+import os
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+from time import sleep
+
+os.chdir('F:\\Dropbox\\IO-CEL\\data and code\\CELu codes\\Datastream\\Request tables\\')
+
+path="F:\\Dropbox\\IO-CEL\\data and code\\CELu codes\\Datastream\\Request tables\\"
+
+# Open up Excel and make it visible
+excel = win32.Dispatch('Excel.Application')
+excel.Visible = True
+
+file="RequestTable.xlsm"  #request table template
+
+workbook = excel.Workbooks.Open(path+file)
+
+variable='Price'
+
+frequency='Daily'
+
+start_year="1995"
+
+end_year="2021"
+
+
+# download daily return by year
+
+for year in range(start_year,end_year):
+    
+  workbook = excel.Workbooks.Open(path+file)
+
+  RequestTable=workbook.Worksheets("REQUEST_TABLE") 
+
+  RequestTable.Range("B7:B22").Value="Y"  
+  
+  RequestTable.Range("F7:F22").Value=variable  
+  
+  RequestTable.Range("I7:I22").Value=frequency  
+
+  RequestTable.Range("G7:G22").Value=start_date
+
+  RequestTable.Range("H7:H22").Value=end_date
+
+   # invoke macro
+
+  workbook.Application.Run("ProcessRequestTable")
+
+  workbook.SaveAs(path+variable+"_daily_{0}.xlsm".format(year))
+  
+  workbook.Close()
+  
+  sleep(10) # so that it doesn't crash?
+   
+  #excel.Application.Quit()
+
---
+++ request_table_to_record_form.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 22 08:31:38 2021
+
+@author: lucielu
+
+This program transforms Datastream request table to record form long table
+"""
+
+import os
+import pandas as pd
+import numpy as np
+import datetime
+
+path='F:/Dropbox/IO-CEL/data and code/CELu codes/Datastream/request tables/'
+
+#path='/Users/yiliulu/Dropbox/IO-CEL/data and code/CELu codes/python'
+
+os.chdir(path)
+
+#variable='Analyst'
+#variable='ADR'
+
+def dstorecord(variable):
+    
+    table=pd.ExcelFile('F:/Dropbox/IO-CEL/data and code/CELu codes/Datastream/Request tables/{0}.xlsm'.format(variable))
+    n=len(table.sheet_names)
+    
+    for i in range(1,n):
+    
+      xl=pd.read_excel('F:/Dropbox/IO-CEL/data and code/CELu codes/Datastream/Request tables/{0}.xlsm'.format(variable),sheet_name='Sheet{0}'.format(i), converters={'INDICATOR ADR':str,'INDICATOR - ADR':str})
+      print(len(xl))
+      
+      if variable=='ADR':
+          xl=xl.assign(ADR=lambda x: 1*(x.iloc[:,1]=='X')+1*(x.iloc[:,2]=='X'))
+          xl.rename(columns={'Type':'DSCD'},inplace=True)
+          xl=xl[['DSCD','ADR']]
+      else:    
+          xl=pd.melt(xl,value_name=variable,var_name='date',value_vars=xl.columns.drop(['Name','DSCD']),id_vars='DSCD')
+          xl[variable]=pd.to_numeric(xl[variable],errors='coerce')
+          xl['date']=xl['date'].astype('int64')
+      xl=xl.dropna()
+      
+      if i==1:
+        mergexl=xl
+    
+    #convert DS string NA to python missing values
+      else:
+        mergexl=mergexl.append(xl,sort=False)
+
+    
+    mergexl.to_csv('F:/Dropbox/IO-CEL/data and code/CELu codes/Datastream/Request tables/{0}.csv'.format(variable),index=False) 
+
